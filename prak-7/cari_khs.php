00397f88374ffcbac8f156443ee9de8f66bd706a
Dellyvia Elida <dellyvia1900018055@webmail.uad.ac.id>
2021-12-04 20:42:02 +0700

    post 7

--- prak-7/cari_khs.php
+++ prak-7/cari_khs.php
@@ -21,17 +21,24 @@ if(isset($_GET['cari'])){
     <tr>
         <th>No</th>
         <th>NIM</th>
+        <th>Nama Mahasiswa</th>
         <th>Kode MK</th>
+        <th>Nama MK</th>
         <th>Nilai</th>
     </tr>
     <?php 
     if(isset($_GET['cari'])){
         $cari = $_GET['cari'];
-        $sql="select * from KHS where nim = ' ".$cari." '";
+        $sql = "select * from KHS 
+                inner join mahasiswa on KHS.nim = mahasiswa.nim
+                inner join matakuliah on KHS.kode_MK = matakuliah.kode
+                where KHS.nim = '".$cari."'";
         $tampil = mysqli_query($con,$sql);
     }
     else{
-        $sql="select * from KHS";
+        $sql = "select * from KHS 
+                inner join mahasiswa on KHS.nim = mahasiswa.nim
+                inner join matakuliah on KHS.kode_MK = matakuliah.kode";
         $tampil = mysqli_query($con,$sql);
     }
     $no = 1;
@@ -40,7 +47,9 @@ if(isset($_GET['cari'])){
     <tr>
         <td><?php echo $no++; ?></td>
         <td><?php echo $r['nim']; ?></td>
+        <td><?php echo $r['nama']; ?></td>
         <td><?php echo $r['kode_MK']; ?></td>
+        <td><?php echo $r['nama_MK']; ?></td>
         <td><?php echo $r['nilai']; ?></td>
     </tr>
     <?php } ?>
